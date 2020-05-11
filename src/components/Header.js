e57c519e6eee3b016f066e7aff4e6701aac282ef
Krisi <kpucu9@gmail.com>
2020-05-10 21:34:21 -0500

    changed content to about and work page

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ const Header = props => (
               props.onOpenArticle('work')
             }}
           >
-            Work
+            Skills
           </button>
         </li>
         <li>
@@ -38,7 +38,7 @@ const Header = props => (
               props.onOpenArticle('about')
             }}
           >
-            About
+            Work
           </button>
         </li>
         <li>
